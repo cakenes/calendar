cbf879d07d3243282cc6928da6756fbcca6704d3
cakenes <jyri.kurtakko@gmail.com>
2024-03-24 13:13:24 +0200

    Add middlewares and calendar functions

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"database/sql"
 	"log"
+	"main/middleware"
 	"main/models"
+	"math"
 	"net/http"
 	"text/template"
+	"time"
 )
 
 var DB *sql.DB
@@ -13,6 +16,80 @@ var renderer *TemplateRenderer
 
 type TemplateRenderer struct {
 	templates *template.Template
+}
+
+func Weekdays(startDay int) []string {
+	days := []string{"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"}
+	adjustedDays := append(days[startDay:], days[:startDay]...)
+	return adjustedDays
+}
+
+func WeekNumbers(year, month, startDay int) map[int]int {
+	weekNumbers := make(map[int]int)
+	firstDay := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+
+	offset := int(firstDay.Weekday()) - startDay
+	if offset < 0 {
+		offset += 7
+	}
+	adjustedFirstDay := firstDay.AddDate(0, 0, -offset)
+	_, week := adjustedFirstDay.ISOWeek()
+
+	for i := 2; i <= 7; i++ {
+		weekNumbers[i] = week + i - 2
+	}
+
+	yearWeeks := weeksInYear(year) // Rolling over to the next year
+	for i := 2; i <= 7; i++ {
+		if weekNumbers[i] > yearWeeks {
+			weekNumbers[i] = weekNumbers[i] - yearWeeks
+		}
+	}
+
+	return weekNumbers
+}
+
+func weeksInYear(year int) int {
+	lastDay := time.Date(year, time.December, 31, 0, 0, 0, 0, time.UTC)
+	_, week := lastDay.ISOWeek()
+	if week == 1 {
+		return 52
+	}
+	return week
+}
+
+func DaysFromPreviousMonth(year, month, startDay int) []int {
+	firstDay := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+	startDayOffset := (7 - startDay + int(firstDay.Weekday())) % 7
+	prevMonthLastDay := firstDay.AddDate(0, 0, -1)
+	prevMonthNumDays := prevMonthLastDay.Day()
+
+	daysFromPrevMonth := make([]int, startDayOffset)
+	for i := 0; i < startDayOffset; i++ {
+		daysFromPrevMonth[i] = prevMonthNumDays - startDayOffset + i + 1
+	}
+	return daysFromPrevMonth
+}
+
+func DaysFromCurrentMonth(year, month int) []int {
+	firstDay := time.Date(year, time.Month(month), 1, 0, 0, 0, 0, time.UTC)
+	lastDay := firstDay.AddDate(0, 1, -1)
+	numDays := lastDay.Day()
+
+	daysFromCurrentMonth := make([]int, numDays)
+	for i := 0; i < numDays; i++ {
+		daysFromCurrentMonth[i] = i + 1
+	}
+	return daysFromCurrentMonth
+}
+
+func DaysFromNextMonth(prev, current int) []int {
+	remaining := int(math.Max(0, float64(42-prev-current)))
+	daysFromNextMonth := make([]int, remaining)
+	for i := range daysFromNextMonth {
+		daysFromNextMonth[i] = i + 1
+	}
+	return daysFromNextMonth
 }
 
 func (t *TemplateRenderer) Render(w http.ResponseWriter, name string, data interface{}) error {
@@ -26,10 +103,21 @@ func main() {
 	defer models.DB.Close()
 
 	renderer = &TemplateRenderer{
-		templates: template.Must(template.ParseGlob("templates/*.html")),
+		templates: template.Must(template.ParseGlob("templates/*.gohtml")),
 	}
 
 	router := http.NewServeMux()
+
+	year, month, day := time.Now().Date()
+	_, week := time.Now().ISOWeek()
+
+	offset := 1
+
+	weekdays := Weekdays(offset)
+	weeks := WeekNumbers(year, int(month), offset)
+	prevDays := DaysFromPreviousMonth(year, int(month), offset)
+	days := DaysFromCurrentMonth(year, int(month))
+	nextDays := DaysFromNextMonth(len(prevDays), len(days))
 
 	router.Handle("/css/", http.StripPrefix("/css/", http.FileServer(http.Dir("templates/css"))))
 
@@ -39,14 +127,20 @@ func main() {
 		}
 	})
 
-	router.HandleFunc("/content", func(w http.ResponseWriter, r *http.Request) {
-		if err := renderer.Render(w, "content", nil); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-	})
-
 	router.HandleFunc("/calendar", func(w http.ResponseWriter, r *http.Request) {
-		if err := renderer.Render(w, "calendar", nil); err != nil {
+		data := map[string]interface{}{
+			"day":      day,
+			"month":    month,
+			"year":     year,
+			"weekdays": weekdays,
+			"week":     week,
+			"weeks":    weeks,
+			"prevDays": prevDays,
+			"days":     days,
+			"nextDays": nextDays,
+		}
+
+		if err := renderer.Render(w, "calendar", data); err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 		}
 	})
@@ -57,9 +151,11 @@ func main() {
 		}
 	})
 
+	middleware := middleware.CombineMiddlewares(middleware.Logging)
+
 	server := http.Server{
 		Addr:    ":8080",
-		Handler: router,
+		Handler: middleware(router),
 	}
 
 	log.Fatal(server.ListenAndServe())
